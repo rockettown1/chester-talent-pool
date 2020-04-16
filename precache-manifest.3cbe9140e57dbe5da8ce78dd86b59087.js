6962db10f69b58af3b1f78c1075a01a4a20e1cbd
Dan Krishnan <dan@wearecodenation.com>
2020-04-16 01:58:52 +0100

    Updates

---
+++ precache-manifest.3cbe9140e57dbe5da8ce78dd86b59087.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5334a5bf8074d17bb8525446629759b5",
+    "url": "/devdisplay/index.html"
+  },
+  {
+    "revision": "0406df74000ea49df60b",
+    "url": "/devdisplay/static/css/main.793c6ab0.chunk.css"
+  },
+  {
+    "revision": "2f48e82ff64cb5ea89eb",
+    "url": "/devdisplay/static/js/2.86e39950.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/devdisplay/static/js/2.86e39950.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0406df74000ea49df60b",
+    "url": "/devdisplay/static/js/main.4371ca7a.chunk.js"
+  },
+  {
+    "revision": "342d2abf1453668615c4",
+    "url": "/devdisplay/static/js/runtime-main.fe82374d.js"
+  },
+  {
+    "revision": "ba18ae4c82e4c58ac08b1a4e6ba33768",
+    "url": "/devdisplay/static/media/GaMingChan.ba18ae4c.jpg"
+  },
+  {
+    "revision": "5fc3960258b439ddc6a9d99b8b44cad3",
+    "url": "/devdisplay/static/media/Matt.5fc39602.jpg"
+  },
+  {
+    "revision": "58995acce406af88bcb00858b3e05520",
+    "url": "/devdisplay/static/media/Mike.58995acc.jpg"
+  }
+]);
