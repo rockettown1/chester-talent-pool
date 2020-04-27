9fea3a44498f862bd224ecaf7cd482ba989fc6ea
Dan Krishnan <dan@wearecodenation.com>
2020-04-27 21:12:11 +0100

    Updates

--- precache-manifest.2a44b6ea752fbc19b2251471c413ff4a.js
+++ precache-manifest.2a44b6ea752fbc19b2251471c413ff4a.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "21d868eeb552be7ba5175a151270d725",
+    "revision": "3ca95f6aa38be2683b95941b8f9ad707",
     "url": "/chester-talent-pool/index.html"
   },
   {
-    "revision": "6f348e808ade771660ba",
+    "revision": "184465a49f6513156f80",
     "url": "/chester-talent-pool/static/css/main.793c6ab0.chunk.css"
   },
   {
-    "revision": "12b54219dbe8fbd8408e",
-    "url": "/chester-talent-pool/static/js/2.6e013429.chunk.js"
+    "revision": "270e685aa4fecc0bb7a3",
+    "url": "/chester-talent-pool/static/js/2.5f017a61.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/chester-talent-pool/static/js/2.6e013429.chunk.js.LICENSE.txt"
+    "url": "/chester-talent-pool/static/js/2.5f017a61.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6f348e808ade771660ba",
-    "url": "/chester-talent-pool/static/js/main.fc35529e.chunk.js"
+    "revision": "184465a49f6513156f80",
+    "url": "/chester-talent-pool/static/js/main.88b5444a.chunk.js"
   },
   {
     "revision": "c606e104ec6a965d8aac",
@@ -36,7 +36,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/chester-talent-pool/static/media/Mike.58995acc.jpg"
   },
   {
+    "revision": "d896686d2b6433cfd835d23316a08c65",
+    "url": "/chester-talent-pool/static/media/github.d896686d.svg"
+  },
+  {
     "revision": "76322284799031ac18193592ab35fe9c",
     "url": "/chester-talent-pool/static/media/liam.76322284.jpg"
+  },
+  {
+    "revision": "ab88cf0a87726f80ffb84eca7437842a",
+    "url": "/chester-talent-pool/static/media/linkedin.ab88cf0a.svg"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/chester-talent-pool/precache-manifest.c05a3ca0df9da15b7923cc488d66fd10.js"
+  "/chester-talent-pool/precache-manifest.2a44b6ea752fbc19b2251471c413ff4a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/chester-talent-pool/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
